7383aa77e3382952bac420d8f8eba261b547cbb9
Jorge Vicens Valor <drzoid@localhost.localdomain>
2020-04-02 22:24:32 +0200

    🚧 finished one component per file

--- src/person.js
+++
@@ -1,7 +0,0 @@
-const isAdult = (age) => age >= 18 ? true : false;
-const canDrink = (age) => age >= 16 ? true : false;
-
-
-export default (age) => age >= 0 ? true : false;
-export { isAdult, canDrink }
-

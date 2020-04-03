c6e5ab012eb547f03dfb005b9de1d2394c0d1c18
Jorge Vicens Valor <drzoid@localhost.localdomain>
2020-04-03 09:51:53 +0200

    🚧 finished setting up react-modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -4,11 +4,13 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 
 class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     };
 
     componentDidMount() {
@@ -50,7 +52,9 @@ class IndecisionApp extends React.Component {
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option);
+        this.setState(() => ({
+            selectedOption: option
+        }))
     };
 
     handleAddOption =(option) => {
@@ -61,6 +65,10 @@ class IndecisionApp extends React.Component {
         };
 
         this.setState((prevState)=> ({ options: prevState.options.concat([option])}));
+    };
+
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }))
     };
 
     render() {
@@ -84,6 +92,9 @@ class IndecisionApp extends React.Component {
                 <AddOption
                     handleAddOption={this.handleAddOption}
                 />
+                <OptionModal
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}/>
             </div>
         );
     };

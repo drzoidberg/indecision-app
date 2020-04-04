d50b70bfe5747b162a7963a1b931907a11825695
Jorge Vicens Valor <drzoid@localhost.localdomain>
2020-04-04 19:53:15 +0200

    :construction: finished setting up webpack with scss

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
 
+import './styles/styles.scss'
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,25 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
+
         contentBase: path.join(__dirname, 'public')
     }
 };
